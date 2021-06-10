b36a44a0a1c1ae08e32dd68eaa113de78f2b703a
Víctor Molina García <molinav@users.noreply.github.com>
2021-06-10 20:30:06 +0200

    Update PyEnv installation for PyEnv 2.x

--- scripts/install_pyenv_python.sh
+++ scripts/install_pyenv_python.sh
@@ -8,20 +8,22 @@ unzip -q pyenv.zip pyenv-master/* && mv pyenv-master $PYENV_ROOT
 rm -f pyenv.zip
 
 # Link to the old OpenSSL if installed (i.e. if old Python version).
-openssl_dir=$(find /opt -maxdepth 1 -type d -name "*ssl*" | head -n1)
-if [ "$openssl_dir" != "" ]; then
-    export CFLAGS="-I$openssl_dir/include"
-    export LDFLAGS="-L$openssl_dir/lib"
-    export LD_LIBRARY_PATH="$openssl_dir/lib"
+prefix=$(find /opt -maxdepth 1 -type d -name "*ssl*" | head -n1)
+if [ "${prefix}" != "" ]; then
+    export CFLAGS="-I${prefix}/include"
+    export LDFLAGS="-L${prefix}/lib"
 fi
-
-# Initialise PyEnv and install a specific Python version.
-eval "$(pyenv init -)"
-pyenv install "$version"
 
 # Add PyEnv + Python initialisation to profile.
 rc3=/etc/profile.d/03-set-pyenv.sh
-echo "# Enable PyEnv environment" >> $rc3
-echo 'eval "$(pyenv init -)"' >> $rc3
-echo "pyenv shell $version" >> $rc3
-echo "" >> $rc3
+echo "# Enable PyEnv environment" > ${rc3}
+echo 'export PYENV_ROOT="/usr/local/share/pyenv"' >> ${rc3}
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${rc3}
+echo 'eval "$(pyenv init --path)"' >> ${rc3}
+echo 'eval "$(pyenv init -)"' >> ${rc3}
+echo "" >> ${rc3}
+
+# Initialise PyEnv and install a specific Python version.
+. ${rc3}
+pyenv install "${version}"
+echo "pyenv shell ${version}" >> ${rc3}
